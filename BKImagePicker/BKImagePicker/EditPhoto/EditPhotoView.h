3d70d1cf3f4fbc78986e09266c51ca79a752dfa0
FOREVERIDIOT <694092596@qq.com>
2017-01-18 16:18:13 +0800

    no message

---
+++ BKImagePicker/BKImagePicker/EditPhoto/EditPhotoView.h
@@ -0,0 +1,13 @@
+//
+//  EditPhotoView.h
+//  BKImagePicker
+//
+//  Created by iMac on 17/1/18.
+//  Copyright © 2017年 BIKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditPhotoView : UIView
+
+@end
---
+++ BKImagePicker/BKImagePicker/EditPhoto/EditPhotoView.m
@@ -0,0 +1,15 @@
+//
+//  EditPhotoView.m
+//  BKImagePicker
+//
+//  Created by iMac on 17/1/18.
+//  Copyright © 2017年 BIKE. All rights reserved.
+//
+
+#import "EditPhotoView.h"
+
+@implementation EditPhotoView
+
+
+
+@end
