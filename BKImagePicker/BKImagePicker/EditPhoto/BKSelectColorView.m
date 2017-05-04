50c771229bbd612a81d49f24e7b5307843791e4f
FOREVERIDIOT <694092596@qq.com>
2017-05-04 18:47:26 +0800

    no message

--- BKImagePicker/BKImagePicker/EditPhoto/BKSelectColorView.m
+++ BKImagePicker/BKImagePicker/EditPhoto/BKSelectColorView.m
@@ -7,9 +7,98 @@
 //
 
 #import "BKSelectColorView.h"
+#import "BKImagePickerConst.h"
 
 @implementation BKSelectColorView
 
-//-(instancetype)
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        self.backgroundColor = [UIColor clearColor];
+        
+        [self createColorBtn];
+    }
+    return self;
+}
+
+-(void)createColorBtn
+{
+    for (int i = 0; i < 10 ; i++) {
+        UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake(UISCREEN_WIDTH/8, 0, UISCREEN_WIDTH/2, self.bk_height);
+        [button addTarget:self action:@selector(buttonDown:) forControlEvents:UIControlEventTouchDown];
+        [button addTarget:self action:@selector(buttonDragIn:) forControlEvents:UIControlEventTouchDragInside];
+        button.tag = i+1;
+        [self addSubview:button];
+        
+        UIImageView * colorImageView = [[UIImageView alloc]initWithFrame:button.bounds];
+        switch (i) {
+            case 0:
+            {
+                colorImageView.backgroundColor = [UIColor redColor];
+            }
+                break;
+            case 1:
+            {
+                colorImageView.backgroundColor = [UIColor orangeColor];
+            }
+                break;
+            case 2:
+            {
+                colorImageView.backgroundColor = [UIColor yellowColor];
+            }
+                break;
+            case 3:
+            {
+                colorImageView.backgroundColor = [UIColor greenColor];
+            }
+                break;
+            case 4:
+            {
+                colorImageView.backgroundColor = [UIColor blueColor];
+            }
+                break;
+            case 5:
+            {
+                colorImageView.backgroundColor = [UIColor purpleColor];
+            }
+                break;
+            case 6:
+            {
+                colorImageView.backgroundColor = [UIColor blackColor];
+            }
+                break;
+            case 7:
+            {
+                colorImageView.backgroundColor = [UIColor whiteColor];
+            }
+                break;
+            case 8:
+            {
+                colorImageView.backgroundColor = [UIColor lightGrayColor];
+            }
+                break;
+            case 9:
+            {
+                
+            }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+-(void)buttonDown:(UIButton*)button
+{
+    
+}
+
+-(void)buttonDragIn:(UIButton*)button
+{
+    
+}
 
 @end
