dcf247625288401e3c8cbce2d5a8a74e475ea056
FOREVERIOIOT <694092596@qq.com>
2018-04-19 15:58:47 +0800

    解决一个小bug

--- BKImagePicker/BKImagePicker/Util/Base/BKImageBaseViewController.m
+++ BKImagePicker/BKImagePicker/Util/Base/BKImageBaseViewController.m
@@ -21,6 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.view.clipsToBounds = YES;
     self.view.backgroundColor = [UIColor whiteColor];
     self.automaticallyAdjustsScrollViewInsets = NO;
     
